118da8f123d5dedf36e237b214f12db485dc4df7
Hien Nguyen <ktsthehien@gmail.com>
2021-02-03 00:15:25 -0800

    detail commit

--- club/urls.py
+++ club/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('resources/', views.resources, name='resources')
+    path('resources/', views.resources, name='resources'),
+    path('meeting/', views.meeting, name='meeting'),
+    path('meetingdetail/<int:id>', views.meetingdetail, name='detail')
 ]
